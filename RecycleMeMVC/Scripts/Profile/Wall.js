27fdb4075766431f9108c351629f2b66ca0914f1
hvillacruz <hvillacruz@gmail.com>
2014-04-04 11:58:10 +0800

    wall

---
+++ RecycleMeMVC/Scripts/Profile/Wall.js
@@ -0,0 +1,22 @@
+﻿var WallViewModel = function () {
+
+    var self = this;
+    this.Info = ko.observableArray();
+    this.Wall = function () {
+
+        //AjaxNinja.Invoke(ODataApi.User + "('" + $("#currentUser").data("text") + "')" + "?$expand=UserFollowerUsers,UserCommenter,UserFollowing", "GET", {}, function (data) {
+        //    self.Info(data);
+        //});
+        var data = { title: "Hello Wall" };
+        self.Info(data);
+    }
+
+
+
+
+
+
+}
+var wall = new WallViewModel();
+ko.applyBindings(wall, document.getElementById("wallDiv"));
+wall.Wall();
