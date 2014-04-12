121b5ea0b779561ecdbeb94759bff4de015bc800
hvillacruz <hvillacruz@gmail.com>
2014-04-13 07:37:46 +0800

    Updated

---
+++ RecycleMeMVC/Scripts/Main/Global.js
@@ -0,0 +1,36 @@
+﻿var GlobalViewModel = function () {
+
+    var self = this;
+    self.Login = ko.observableArray();
+    this.User = function (UserId, UserName) {
+
+        this.User.UserId = ko.observable(UserId);
+        this.User.UserName = ko.observable(UserName);
+
+    },
+
+    this.Profile = function () {
+
+        if (BrowserDetect.browser != "Safari") {
+            ODataNinja.Read(ODataApi.User + "('" + this.User.UserId() + "')", function (data) {
+                self.Login(data);
+                self.AttachEvents();
+            });
+        } else {
+            AjaxNinja.Invoke(ODataApi.User + "('" + this.User.UserId() + "')", "GET", {}, function (data) {
+                self.Login(data);
+                self.AttachEvents();
+            });
+
+        }
+
+    }
+
+    this.AttachEvents = function () {
+
+        var header = new Header();
+        header.Logout();
+
+    }
+
+}
