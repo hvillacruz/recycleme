d8a4747d0089ea9809dc94a7ad42738af3b5c93d
hvillacruz <hvillacruz@gmail.com>
2014-05-10 10:19:35 +0800

    ko tradeitem sit

---
+++ RecycleMeMVC/Scripts/Home/TradeItems.js
@@ -0,0 +1,17 @@
+﻿var TradeItemsViewModel = function () {
+
+    var self = this;
+    this.Items = ko.observableArray();
+    this.TradeItem = function () {
+       
+        AjaxNinja.Invoke(ODataApi.Item + "?$expand=ItemImages", "GET", {}, function (data) {
+            self.Items(data.value);
+        });
+
+    }
+
+   
+}
+var trade = new TradeItemsViewModel();
+ko.applyBindings(trade, document.getElementById("grid"));
+trade.TradeItem();
