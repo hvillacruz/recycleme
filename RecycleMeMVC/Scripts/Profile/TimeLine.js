20080a216299d865139cd713caf618c885d1938d
hvillacruz <hvillacruz@gmail.com>
2014-05-20 11:25:30 +0800

    update data

--- RecycleMeMVC/Scripts/Profile/TimeLine.js
+++ RecycleMeMVC/Scripts/Profile/TimeLine.js
@@ -23,39 +23,19 @@
 
     function setEditable() {
         $.fn.editable.defaults.mode = 'inline';
-        $('.editable').editable('submit', {
-            url: '/newuser',
-            ajaxOptions: {
-                dataType: 'json' //assuming json response
-            },
-            success: function (data, config) {
-                alert('ss');
-                if (data && data.id) {  //record created, response like {"id": 2}
-                    //set pk
-                    $(this).editable('option', 'pk', data.id);
-                    //remove unsaved class
-                    $(this).removeClass('editable-unsaved');
-                    //show messages
-                    var msg = 'New user created! Now editables submit individually.';
-                    $('#msg').addClass('alert-success').removeClass('alert-error').html(msg).show();
-                    $('#save-btn').hide();
-                    $(this).off('save.newuser');
-                } else if (data && data.errors) {
-                    //server-side validation error, response like {"errors": {"username": "username already exist"} }
-                    config.error.call(this, data.errors);
-                }
-            },
-            error: function (errors) {
-                alert('error');
-                var msg = '';
-                if (errors && errors.responseText) { //ajax error, errors = xhr object
-                    msg = errors.responseText;
-                } else { //validation error (client-side or server-side)
-                    $.each(errors, function (k, v) { msg += k + ": " + v + "<br>"; });
-                }
-                $('#msg').removeClass('alert-success').addClass('alert-error').html(msg).show();
+        $('.editable').editable();
+
+        $('.editable').on('save', function (e, params) {
+
+            var item = {
+                CommentText: params.newValue,
+                Id: $(this).data('editable').options.pk
             }
+
+            self.ModifyComment(item);
         });
+
+
     }
 
     this.ShowComment = function (currentItem, selectedImage) {
@@ -85,7 +65,7 @@
         }
 
         AjaxNinja.Invoke(ODataApi.ItemComment + "(" + item.Id + ")", "PATCH", JSON.stringify(data), function (result) {
-            alert(result);
+            //alert(result);
         });
     }
 
