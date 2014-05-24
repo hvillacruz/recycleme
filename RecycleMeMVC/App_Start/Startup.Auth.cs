4763585888356a812e9d00323f5ac0d3fd1c5ed7
hvillacruz <hvillacruz@gmail.com>
2014-05-25 00:06:58 +0800

    facebook options

--- RecycleMeMVC/App_Start/Startup.Auth.cs
+++ RecycleMeMVC/App_Start/Startup.Auth.cs
@@ -29,11 +29,86 @@ namespace RecycleMeMVC
             //    clientSecret: "");
 
 
-            List<string> scope = new List<string>() { "email", "user_about_me", "user_hometown", "friends_about_me", "friends_photos" };
+            List<string> scope = new List<string>() { "email", "user_about_me", "user_hometown", "friends_about_me", "friends_photos", "manage_pages", "publish_actions", "publish_stream" };
             var x = new FacebookAuthenticationOptions();
             x.Scope.Add("user_birthday");
             x.Scope.Add("user_location");
             x.Scope.Add("email");
+            x.Scope.Add("publish_actions");
+            x.Scope.Add("manage_pages");
+            x.Scope.Add("publish_stream");
+            x.Scope.Add("user_notes");
+            x.Scope.Add("user_online_presence");
+            x.Scope.Add("user_photo_video_tags");
+            x.Scope.Add("user_photos");
+            x.Scope.Add("user_questions");
+            x.Scope.Add("user_relationship_details");
+            x.Scope.Add("user_relationships");
+            x.Scope.Add("user_religion_politics");
+            x.Scope.Add("user_status");
+            x.Scope.Add("user_subscriptions");
+            x.Scope.Add("user_videos");
+            x.Scope.Add("user_website");
+            x.Scope.Add("user_work_history");
+
+            x.Scope.Add("friends_about_me");
+            x.Scope.Add("friends_actions.books");
+            x.Scope.Add("friends_actions.music");
+            x.Scope.Add("friends_actions.news");
+            x.Scope.Add("friends_actions.video");
+            x.Scope.Add("friends_activities");
+            x.Scope.Add("friends_birthday");
+            x.Scope.Add("friends_checkins");
+            x.Scope.Add("friends_education_history");
+            x.Scope.Add("friends_events");
+            x.Scope.Add("friends_games_activity");
+            x.Scope.Add("friends_groups");
+            x.Scope.Add("friends_hometown");
+            x.Scope.Add("friends_interests");
+            x.Scope.Add("friends_likes");
+            x.Scope.Add("friends_location");
+            x.Scope.Add("friends_notes");
+            x.Scope.Add("friends_online_presence");
+            x.Scope.Add("friends_photo_video_tags");
+            x.Scope.Add("friends_photos");
+            x.Scope.Add("friends_questions");
+            x.Scope.Add("friends_relationship_details");
+            x.Scope.Add("friends_relationships");
+            x.Scope.Add("friends_religion_politics");
+            x.Scope.Add("friends_status");
+            x.Scope.Add("friends_subscriptions");
+            x.Scope.Add("friends_videos");
+            x.Scope.Add("friends_website");
+            x.Scope.Add("friends_work_history");
+
+            x.Scope.Add("ads_management");
+            x.Scope.Add("ads_read");
+            x.Scope.Add("create_event");
+            x.Scope.Add("create_note");
+            x.Scope.Add("email");
+            x.Scope.Add("export_stream");
+            x.Scope.Add("manage_friendlists");
+            x.Scope.Add("manage_notifications");
+            x.Scope.Add("manage_pages");
+            x.Scope.Add("photo_upload");
+            x.Scope.Add("publish_actions");
+            x.Scope.Add("publish_checkins");
+            x.Scope.Add("publish_stream");
+            x.Scope.Add("read_friendlists");
+            x.Scope.Add("read_insights");
+            x.Scope.Add("read_mailbox");
+            x.Scope.Add("read_page_mailboxes");
+            x.Scope.Add("read_requests");
+            x.Scope.Add("read_stream");
+            x.Scope.Add("rsvp_event");
+            x.Scope.Add("share_item");
+            x.Scope.Add("sms");
+            x.Scope.Add("status_update");
+            x.Scope.Add("video_upload");
+            x.Scope.Add("xmpp_login");
+
+
+
 
             x.AppId = "226723064158229";
             x.AppSecret = "57b65d4b283785e7b5ee9e0af93ab3e8";
--- RecycleMeMVC/Controllers/ProfileController.cs
+++ RecycleMeMVC/Controllers/ProfileController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using RecycleMeMVC.Models;
+using Facebook;
+using System.Configuration;
 namespace RecycleMeMVC.Controllers
 {
     [RoutePrefix("Profile")]
@@ -21,5 +23,73 @@ namespace RecycleMeMVC.Controllers
         }
 
 
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
+        public ActionResult Facebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = ConfigurationManager.AppSettings["FbAppId"],
+                client_secret = ConfigurationManager.AppSettings["FbAppSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                 grant_type = "authorization_code",
+                response_type = "code",
+                scope = "email" // Add other permissions as needed
+            });
+
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+
+
+
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id =  ConfigurationManager.AppSettings["FbAppId"],
+                client_secret =  ConfigurationManager.AppSettings["FbAppSecret"],
+                redirect_uri = RedirectUri.AbsoluteUri,
+                grant_type = "authorization_code",
+                code = code
+            });
+
+
+
+
+            var accessToken = result.access_token;
+
+            // Store the access token in the session
+            Session["AccessToken"] = accessToken;
+
+            // update the facebook client with the access token so 
+            // we can make requests on behalf of the user
+            fb.AccessToken = accessToken;
+
+            // Get the user's information
+            dynamic me = fb.Get("me?fields=first_name,last_name,id,email");
+            string email = me.email;
+            dynamic res = fb.Post("me/feed", new
+            {
+                message = "My first wall post using Facebook SDK for .NET"
+            });
+            var newPostId = res.id;
+
+            // Set the auth cookie
+            //FormsAuthentication.SetAuthCookie(email, false);
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
