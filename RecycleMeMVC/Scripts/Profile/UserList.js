f9a726ac6958d929c79ca9b64cea22ee8e0b1203
hvillacruz <hvillacruz@gmail.com>
2014-06-11 15:18:09 +0800

    navigate user

--- RecycleMeMVC/Scripts/Profile/UserList.js
+++ RecycleMeMVC/Scripts/Profile/UserList.js
@@ -5,17 +5,23 @@
     this.Type = ko.observable("Following");
     this.Items = ko.observableArray();
     this.Users = function () {
-        
+
         var result = window.location.href.indexOf("Followers") < 0 ? this.Type("Following") : this.Type("Follower");
         var type = window.location.href.indexOf("Followers") < 0 ? "/UserFollowers?$expand=FollowedUser" : "/UserFollowerUsers?$expand=Follower";
         AjaxNinja.Invoke(ODataApi.User + "('" + global.User.UserId() + "')" + type, "GET", {}, function (data) {
-            self.MessageCount( data.value.length);
+            self.MessageCount(data.value.length);
             self.Items(data.value);
         });
 
     }
 
-   
+    this.SelectedUser = function (item) {
+
+        window.location.href = '/Profile/Dashboard/' + item.FollowedUserId;
+
+    }
+
+
 }
 var user = new UserListViewModel();
 ko.applyBindings(user, document.getElementById("grid"));
