c05cd4181c4c32c25125f4b47f5ce9b58c9898b0
hvillacruz <hvillacruz@gmail.com>
2014-06-11 12:02:07 +0800

    view

--- RecycleMeMVC/Scripts/Profile/UserList.js
+++ RecycleMeMVC/Scripts/Profile/UserList.js
@@ -2,9 +2,11 @@
 
     var self = this;
     this.MessageCount = ko.observable();
+    this.Type = ko.observable("Following");
     this.Items = ko.observableArray();
     this.Users = function () {
-  
+        
+        var result = window.location.href.indexOf("Followers") < 0 ? this.Type("Following") : this.Type("Follower");
         var type = window.location.href.indexOf("Followers") < 0 ? "/UserFollowers?$expand=FollowedUser" : "/UserFollowerUsers?$expand=Follower";
         AjaxNinja.Invoke(ODataApi.User + "('" + global.User.UserId() + "')" + type, "GET", {}, function (data) {
             self.MessageCount( data.value.length);
