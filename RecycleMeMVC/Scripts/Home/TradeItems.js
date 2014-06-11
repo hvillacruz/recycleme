19dbaaf378183166c7274f9924f51c4afadf4a65
hvillacruz <hvillacruz@gmail.com>
2014-06-11 17:23:16 +0800

    search function added

--- RecycleMeMVC/Scripts/Home/TradeItems.js
+++ RecycleMeMVC/Scripts/Home/TradeItems.js
@@ -10,8 +10,32 @@
 
     }
 
+    this.Search = function (item,data) {
+        var result = [];
+        AjaxNinja.Invoke(ODataApi.Item + "?$orderby=ModifiedDate desc&$expand=ItemImages&$filter=substringof('" + item + "',Name) or substringof('" + item + "',TradeTag)", "GET", {}, function (data) {
+          
+            self.Items(data.value);
+
+            setTimeout(function () {
+                var scroll = new AnimOnScroll(document.getElementById('grid'), {
+                    minDuration: 0.4,
+                    maxDuration: 0.7,
+                    viewportFactor: 0.2
+                });
+            }, 100);
+
+        });
+     
+    }
+
+    //self.refresh = function () {
+    //    var data = self.array().slice(0);
+    //    self.array([]);
+    //    self.array(data);
+    //};
+
    
 }
 var trade = new TradeItemsViewModel();
-ko.applyBindings(trade, document.getElementById("grid"));
+ko.applyBindings(trade, document.getElementById("tradeContainer"));
 trade.TradeItem();
