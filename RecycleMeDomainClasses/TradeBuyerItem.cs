886750d863a062231124f2b11df85ced5d1dc3ad
hvillacruz <hvillacruz@gmail.com>
2014-06-19 12:00:28 +0800

    TradeBuyerItem

---
+++ RecycleMeDomainClasses/TradeBuyerItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecycleMeDomainClasses
+{
+    public class TradeBuyerItem :ILogInfo
+    {
+
+        [Key, Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(Order = 1)]
+        public Nullable<long> ItemId { get; set; }
+
+        [Column(Order = 2)]
+        public long TradeId { get; set; }
+
+        public virtual Item Item { get; set; }
+
+        public virtual Trade Trade { get; set; }
+    }
+}
