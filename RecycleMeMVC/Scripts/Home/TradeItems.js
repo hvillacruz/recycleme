12aa72685c1eb3b12a9c73dd86642ffd8e04de25
hvillacruz <hvillacruz@gmail.com>
2014-07-07 16:57:21 +0800

    user link

--- RecycleMeMVC/Scripts/Home/TradeItems.js
+++ RecycleMeMVC/Scripts/Home/TradeItems.js
@@ -3,23 +3,23 @@
     var self = this;
     this.Items = ko.observableArray();
     this.TradeItem = function () {
-       
-        AjaxNinja.Invoke(ODataApi.Item + "?$orderby=ModifiedDate desc&$expand=ItemImages", "GET", {}, function (data) {
+
+        AjaxNinja.Invoke(ODataApi.Item + "?$orderby=ModifiedDate desc&$expand=ItemImages,Owner", "GET", {}, function (data) {
             self.Items(data.value);
             self.Refresh();
         });
 
     }
 
-    this.Search = function (item,data) {
+    this.Search = function (item, data) {
         var result = [];
         AjaxNinja.Invoke(ODataApi.Item + "?$orderby=ModifiedDate desc&$expand=ItemImages&$filter=substringof('" + item + "',Name) or substringof('" + item + "',TradeTag)", "GET", {}, function (data) {
-          
+
             self.Items(data.value);
             self.Refresh();
-           
+
         });
-     
+
     }
 
     this.Refresh = function () {
@@ -32,8 +32,28 @@
         }, 100);
     };
 
-   
+
+    this.SelectedUser = function (item, parent) {
+
+        window.location.href = '/Profile/Dashboard/' + parent.OwnerId;
+
+    }
+
+
+
 }
 var trade = new TradeItemsViewModel();
+
+ko.bindingHandlers.button = {
+    init: function (element) {
+        $(element).click(function () {
+          
+        });
+    }
+
+
+}
+
+
 ko.applyBindings(trade, document.getElementById("tradeContainer"));
 trade.TradeItem();
