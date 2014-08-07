e507d097d1940a1d0429458980768993fc8e6b6a
hvillacruz <hvillacruz@gmail.com>
2014-08-07 19:35:06 +0800

    Exchange Data

---
+++ RecycleMeMVC/Scripts/Profile/exchange.js
@@ -0,0 +1,22 @@
+﻿var ExchangeViewModel = function () {
+
+    var self = this;
+    this.Comment = ko.observableArray();
+    this.Comments = function () {
+
+        AjaxNinja.Invoke(ODataApi.User + "('" + global.User.UserId() + "')" + type, "GET", {}, function (data) {
+
+            self.Comment(data.value);
+
+        });
+
+    }
+
+    this.Buyer = function () {
+
+    }
+
+}
+var exchange = new ExchangeViewModel();
+ko.applyBindings(exchange, document.getElementById("panelContainer"));
+exchange.Comments();
