aa6b998b31817e2c2f9faea00afa04f1bd941ed3
hvillacruz <hvillacruz@gmail.com>
2014-10-28 11:36:59 +0800

    Gallery

--- RecycleMeMVC/Scripts/Home/ItemModalImage.js
+++ RecycleMeMVC/Scripts/Home/ItemModalImage.js
@@ -5,11 +5,31 @@
     this.SelectedItem = ko.observableArray();
     this.ItemImage = function () {
 
-       
+
+
     }
 
-  
- 
+    this.BindImages = function () {
+
+
+        setTimeout(function () {
+
+            $(".iAmTest").nerveSlider({
+                slideTransitionSpeed: 1000,
+                slideTransitionEasing: "easeOutExpo",
+                sliderFullscreen: false,
+                sliderKeepAspectRatio: true,
+                sliderResizable: true,
+                //sliderWidth: "1024px",
+                //sliderHeight: "768px",
+                sliderHeightAdaptable:true
+            });
+
+
+        }, 50);
+
+    }
+
 
 }
 var itemModalImage = new ItemModalImageViewModel();
@@ -18,3 +38,4 @@ var itemModalImage = new ItemModalImageViewModel();
 
 ko.applyBindings(itemModalImage, document.getElementById("itemModalImageContainer"));
 itemModalImage.ItemImage();
+itemModalImage.BindImages();
